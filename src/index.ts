6b1593470518bb997bcd6052ead4d9689202fd47
Rasmus Eneman <rasmus@eneman.eu>
2020-11-22 15:14:35 +0100

    Fix type export not marked as such

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ export { DndProvider, OngoingDrag } from "./context"
 export { createDragType } from "./dragType"
 export type { DragType } from "./dragType"
 export type { EventController, Unsubscribe } from "./events"
-export {
-  SortableList,
-  SortableListProps,
-  useSortabeListItem,
-} from "./SortableList"
+export { SortableList, useSortabeListItem } from "./SortableList"
+export type { SortableListProps } from "./SortableList"
 export { pointerOffset, useDrag } from "./useDrag"
 export { useDrop } from "./useDrop"
