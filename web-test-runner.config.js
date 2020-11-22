3a22dcdbcb76962700814d895450afe2ce9155b0
Rasmus Eneman <rasmus@eneman.eu>
2020-11-22 12:08:06 +0100

    Run tests in CI

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,7 +1,14 @@
+const { chromeLauncher } = require("@web/test-runner")
+
 // NODE_ENV=test - Needed by "@snowpack/web-test-runner-plugin"
 process.env.NODE_ENV = "test"
 
 /** @type {import("@web/test-runner").TestRunnerConfig } */
 module.exports = {
+  browsers: [
+    process.env.CI === "true"
+      ? chromeLauncher({ launchOptions: { args: ["--no-sandbox"] } })
+      : chromeLauncher(),
+  ],
   plugins: [require("@snowpack/web-test-runner-plugin")()],
 }
